c807ca3c9c2e48c95c8755807bbe23e3f8bc5ed5
wangjiong <995204127@163.com>
2018-12-18 20:42:11 +0800

    Add music

--- app/src/main/java/com/wangjiong/ershisishi/MusicService.java
+++ app/src/main/java/com/wangjiong/ershisishi/MusicService.java
@@ -20,6 +20,8 @@ public class MusicService extends Service {
 
     MediaPlayer mMediaPlayer = new MediaPlayer();
 
+    String mCurrentpath;
+
     @Override
     public IBinder onBind(Intent intent) {
         return null;
@@ -28,31 +30,41 @@ public class MusicService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d(TAG, "onCreate");
         msMusicService = this;
-        playMusic("music/main.mp3");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand");
+        MusicService.msMusicService.playMusic("music/main.mp3");
         return START_STICKY;
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.d(TAG, "onDestroy");
     }
 
-    public void playMusic(String path){
+    public void playMusic(String path) {
+        Log.w(TAG, "playMusic : " + path);
+        if (mCurrentpath == path || (mCurrentpath!=null && mCurrentpath.endsWith(path))) {
+            return;
+        }
+        mCurrentpath = path;
         AssetFileDescriptor fileDescriptor;
         try {
             fileDescriptor = getAssets().openFd(path);
+            mMediaPlayer.reset();
             mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
             mMediaPlayer.setDataSource(fileDescriptor.getFileDescriptor(),
                     fileDescriptor.getStartOffset(),
                     fileDescriptor.getLength());
+            mMediaPlayer.setLooping(true);
             mMediaPlayer.prepare();
             mMediaPlayer.start();
-            Log.w(TAG, "mMediaPlayer.start");
+
         } catch (IOException e) {
             e.printStackTrace();
             Log.w(TAG, "IOException");
